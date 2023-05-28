70cdcaa68013e0911fa156817b5494145cb4b4ac
laks2988 <lakshmanapandian.palanikumar@chainsys.com>
2023-05-28 14:35:56 +0530

    Remove the validation temporarly

--- PersonalDocumentManagement/src/app/login/login.component.ts
+++ PersonalDocumentManagement/src/app/login/login.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiServiceService } from '../api-service.service';
-import { FormGroup,FormBuilder ,Validators} from '@angular/forms';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
 @Component({
@@ -10,67 +10,66 @@ import { ToastrService } from 'ngx-toastr';
 })
 
 export class LoginComponent implements OnInit {
-  userForm!:FormGroup;
-  user:any;
-  constructor(private api:ApiServiceService,private formbuilder:FormBuilder,private router:Router,private toastrService: ToastrService) { 
+  userForm!: FormGroup;
+  user: any;
+  constructor(private api: ApiServiceService, private formbuilder: FormBuilder, private router: Router, private toastrService: ToastrService) {
   }
-  user_id:any=[];
-alldata :any;
-flag = 0;
-notify= '';
-object:any=[];
-files:any=[]
+  user_id: any = [];
+  alldata: any;
+  flag = 0;
+  notify = '';
+  object: any = [];
+  files: any = []
   ngOnInit(): void {
     this.getallusers();
     this.userForm = this.formbuilder.group(
       {
-        'username':['',Validators.required],
-        'password':['',Validators.required],
+        'username': ['', Validators.required],
+        'password': ['', Validators.required],
 
       }
     );
   }
-userFormData(formvalue:any){
-  console.log(formvalue);
-  for(const i  of this.object){
-    if(i.username ==  formvalue.username && i.password == formvalue.password){
-      localStorage.setItem('login',JSON.stringify(i));
-      localStorage.setItem('username',JSON.stringify(i._id) );
+  userFormData(formvalue: any) {
+    console.log(formvalue);
+    for (const i of this.object) {
+      if (i.username == formvalue.username && i.password == formvalue.password) {
+        localStorage.setItem('login', JSON.stringify(i));
+        localStorage.setItem('username', JSON.stringify(i._id));
         this.flag = 1;
-        this.api.getusername(i._id).subscribe((data:any)=>{
+        this.api.getusername(i._id).subscribe((data: any) => {
           console.log(data);
         });
-        this.api.getEmail(i.email).subscribe((data:any)=>{
-          console.log("OTP");
-          localStorage.setItem('OTP',JSON.stringify(data));
-          console.log(data);
-        });
+        // this.api.getEmail(i.email).subscribe((data: any) => {
+        //   console.log("OTP");
+        //   localStorage.setItem('OTP', JSON.stringify(data));
+        //   console.log(data);
+        // });
+      }
     }
- }
-  if(this.flag == 1 ){
-    setTimeout(function(){
-      console.log("");
-    }, 10000);
-    this.router.navigate(['/menu/welcomeadmin']);
+    if (this.flag == 1) {
+      setTimeout(function () {
+      }, 1000000000000);
+      this.router.navigate(['/menu/adddocument']);
+    }
+    else {
+      this.toastrService.error("invalid user");
+    }
+
   }
-  else{
-        this.toastrService.error("invalid user") ;
-  }
- 
-}
-getallusers(){
-    this.api.getUser().subscribe(data=>{
+  getallusers() {
+    this.api.getUser().subscribe(data => {
       console.log(data);
       console.log('Data was fetching');
-      this.alldata=data;
+      this.alldata = data;
       console.log(this.alldata);
-      this.alldata=this.alldata.docs;
+      this.alldata = this.alldata.docs;
       console.log(this.alldata);
-      for(const i of this.alldata){
-            this.object.push(i);
+      for (const i of this.alldata) {
+        this.object.push(i);
       }
       console.log('Fetched successfuly');
 
     });
-}
+  }
 }
